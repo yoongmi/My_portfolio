25b5f4ef65a8d8e5e53c0f4cdb268a63133cfe18
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-11-29 23:36:45 +0900

    헤더추가

--- portfolio/src/pages/Header.tsx
+++ portfolio/src/pages/Header.tsx
@@ -1,7 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
+import styled from "styled-components";
 import Logo from "../assets/logo.png";
+import "../style/header.css";
 
 const Header = () => {
+  const homeMatch = useMatch("/");
+  const profileMatch = useMatch("/profile");
+  const workMatch = useMatch("/work");
+
   return (
     <header>
       <h1>
@@ -10,12 +16,34 @@ const Header = () => {
         </Link>
       </h1>
       <ul>
-        <li>
+        <Li isActive={homeMatch !== null}>
           <Link to="/">Home</Link>
-        </li>
+        </Li>
+        <Li isActive={profileMatch !== null}>
+          <Link to="/profile">Profile</Link>
+        </Li>
+        <Li isActive={workMatch !== null}>
+          <Link to="/work">Work</Link>
+        </Li>
       </ul>
     </header>
   );
 };
-
+const Li = styled.li<{ isActive: boolean }>`
+  & a {
+    position: relative;
+    color: ${(props) => props.isActive && "#930909"};
+    /* border-bottom: ${(props) => props.isActive && "2px solid #930909"}; */
+    &:after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: ${(props) => (props.isActive ? "100%" : "0%")};
+      height: 2px;
+      background-color: #930909;
+      transition: width 0.5s;
+    }
+  }
+`;
 export default Header;
