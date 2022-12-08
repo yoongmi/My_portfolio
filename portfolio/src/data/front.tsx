502af652526d0359e3902539ba1949ba03a2d76c
yoongmi <78575434+yoongmi@users.noreply.github.com>
2022-12-08 15:35:26 +0900

    타이틀수정, 프론트리스트 수정

--- portfolio/src/data/front.tsx
+++ portfolio/src/data/front.tsx
@@ -11,14 +11,21 @@ export const project = [
     id: 1,
     title: "넷플릭스 클론",
     thumb: "/frontimg/img01.JPG",
-    skill: ["reactJS", "javascript", "typescript", "Recoil", "scss"],
+    skill: [
+      "reactJS",
+      "javascript",
+      "typescript",
+      "Recoil",
+      "html5",
+      "Styled Component",
+    ],
     link: "https://yoongmi.github.io/My_Netflix/",
   },
   {
     id: 2,
     title: "todoList",
     thumb: "/frontimg/img02.JPG",
-    skill: ["reactJS", "javascript", "typescript", "scss"],
+    skill: ["reactJS", "javascript", "typescript", "html5", "Styled Component"],
     link: "https://yoongmi.github.io/React_todo/",
   },
 ];
